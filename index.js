dffb65184b2d27224bdd6fb91f92216ba83b1d92
Antonina Maj <antoninajanina@gmail.com>
2019-02-04 19:35:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var now = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${now}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var str = "The line is currently: ";
+    for (var i = 0; i < katzDeliLine.length- ; i++) {
+      
+    }
+  }
+}
